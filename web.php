5068fb738435962eb5edf27f281e6e4049220183
LukeMichaels <lmichaels@gmail.com>
2014-03-30 01:06:56 -0700

    major updates to the "web" gallery page.

--- web.php
+++ web.php
@@ -1,141 +1,118 @@
 <!-- :::::::::: Header :::::::::: -->
 <?php include 'includes/header.php'; ?>
 
-<!-- :::::::::: Web Gallery :::::::::: -->
-<div id="gallery" class="js-masonry">
+<div id="full">
+	<p>Here are a few examples of websites that I've designed and developed throughout the years.<br /> 
+	I've worked on countless websites, but these stood out to me for one reason or another.</a></p>
+</div>
 
-	<div id="full">
-		<h1>This page is a work in progress</h1>
-	</div>
+<!-- :::::::::: Web Gallery :::::::::: -->
+<div id="gallery">
+<!-- <div class="section group"> -->
 
 	<div class="col span_1_of_2">
+	
 		<div id="piece-wrapper">
-			<img src="images/retina/rogue-landing-page.jpg" title="Rogue Home Page" alt="Rogue Home Page"><br /><br />
-			<img src="images/retina/rogue-beer-page.jpg" title="Rogue Beer Page" alt="Rogue Beer Page"><br /><br />
+			<a href='images/retina/rogue-landing-page.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/rogue-landing-page.jpg" title="Rogue Home Page" alt="Rogue Home Page">
+			</a><br /><br />
+			<a href='images/retina/rogue-beer-page.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/rogue-beer-page.jpg" title="Rogue Beer Page" alt="Rogue Beer Page">
+			</a><br /><br />
 			<h1>Rogue Design Composition</h1>
 		  	<h3>website design</h3>
-		  	<h4>The Rogue Logo, copy and artwork for Saint Rogue Red are all property of Rogue Ales & Spirits</h4>
-		</div>
-	</div>
-	
-	
-	<div class="col span_1_of_2">
-		<div id="piece-wrapper">
-			<a href="http://blog.bobsredmill.com" alt="Bob's Red Mill Blog" target="blank"><img src="images/retina/bobs-blog.jpg" title="Bob's Red Mill Blog" alt="Bob's Red Mill Blog"></a><br /><br />
-			<h1>Bob's Red Mill Blog</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://blog.bobsredmill.com" alt="Bob's Red Mill Blog" target="blank">http://blog.bobsredmill.com</a></h4>
-		</div>
-	</div>
+			</a>
+	    </div>
 
-  	
-  	<div class="col span_1_of_2">
 		<div id="piece-wrapper">
-			<a href="http://www.bobsredmill.com" alt="Bob's Red Mill" target="blank"><img src="images/retina/bobs-red-mill.jpg" title="Bob's Red Mill" alt="Bob's Red Mill"></a><br /><br />
+			<a href='images/retina/bobs-red-mill.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/bobs-red-mill.jpg" title="Bob's Red Mill" alt="Bob's Red Mill">
+			</a><br /><br />
 			<h1>Bob's Red Mill</h1>
 		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.bobsredmill.com" alt="Bob's Red Mill" target="blank">http://www.bobsredmill.com</a></h4>
+		  	<h4>HTML, CSS, PHP, MySQL, JavaScript, jQuery, X-Cart, Magento</h4>
 		</div>
-  	</div>
+		
+		<div id="piece-wrapper">
+			<a href='images/retina/bobs-red-mill-oatmeal.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/bobs-red-mill-oatmeal.jpg" title="Bob's Red Mill Oatmeal Promo" alt="Bob's Red Mill Oatmeal Promo">
+			</a><br /><br />
+			<h1>Bob's Red Mill Oatmeal Promo</h1>
+		  	<h3>website design/development</h3>
+		  	<h4>HTML, CSS, PHP, MySQL, JavaScript, jQuery</h4>
+		</div>
+
+		 <div id="piece-wrapper">
+			<a href='images/retina/newgameplus.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/newgameplus.jpg" title="NewGame+" alt="NewGame+">
+			</a><br /><br />
+			<h1>NewGame+</h1>
+		  	<h3>website design/development</h3>
+		  	<h4>HTML, CSS, WordPress, PHP, MySQL</h4>
+		</div>	
+		
+		<div id="piece-wrapper">
+			<a href='images/retina/marry-me-moses.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/marry-me-moses.jpg" title="Marry Me Moses" alt="Marry Me Moses">
+			</a><br /><br />
+			<h1>Marry Me Moses</h1>
+		  	<h3>website design/development</h3>
+		  	<h4>HTML, Flash, Action Script</h4>
+		</div>
+		
+	</div><!-- .col span_1_of_2 -->
   	
   	<div class="col span_1_of_2">
+  		
   		<div id="piece-wrapper">
-		  	<img src="images/retina/grains-of-discovery-marquee.jpg" title="Grains of Discovery Marquee" alt="Grains of Discovery Marquee"><br /><br />
-		  	<img src="images/retina/pie-crust-marquee.jpg" title="Gluten Free Pie Crust Mix Marquee" alt="Gluten Free Pie Crust Mix Marquee"><br /><br />
-		  	<img src="images/retina/oatmeal-marquee.jpg" title="World's Best Oatmeal Marquee" alt="World's Best Oatmeal Marquee"><br /><br />
-		  	<!-- <img src="images/retina/Power-Up-With-Chia-Seed.jpg" title="Chia Seed Marquee" alt="Chia Seed Marquee"><br /><br /> -->
-		  	<img src="images/retina/coconut-flour-marquee.jpg" title="Coconut Flour" alt="Coconut Flour Marquee"><br /><br />
-		  	<img src="images/retina/gluten-free-muesli-marquee.jpg" title="Gluten Free Muesli Marquee" alt="Gluten Free Muesli Marquee"><br /><br />
-		  	<img src="images/retina/holiday-marquee.jpg" title="Holiday Marquee" alt="Holiday Marquee"><br /><br />
-			<h1>Bob's Red Mill Marquees</h1>
-		  	<h3>graphic design</h3>
-		  	<h4>Created in Photoshop & Illustrator</h4>
+  			<a href='images/retina/conscious-box-home.jpg' class='fresco' data-fresco-group='web'>
+		  		<img src="images/retina/conscious-box-home.jpg" title="Conscious Box" alt="Conscious Box">
+  			</a><br /><br />
+			<h1>Conscious Box</h1>
+		  	<h3>web development</h3>
+		  	<h4>HTML, CSS, SASS, PHP, JavaScript, jQuery, Magento</h4>
   		</div>
-  	</div>
-  	
-  	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-		  	<img src="images/retina/portland-marathon.jpg" title="Portland Marathon" alt="Portland Marathon"><br /><br />
+		 
+		<div id="piece-wrapper">
+			<a href='images/retina/bobs-blog.jpg' class='fresco' data-fresco-group='web'>
+				<img src="images/retina/bobs-blog.jpg" title="Bob's Red Mill Blog" alt="Bob's Red Mill Blog">
+			</a><br /><br />
+			<h1>Bob's Red Mill Blog</h1>
+		  	<h3>website design/development</h3>
+		  	<h4>HTML, CSS, WordPress, PHP, MySQL</h4>
+		</div> 
+		
+		<div id="piece-wrapper">
+  			<a href='images/retina/bobs-red-mill-pie-page.jpg' class='fresco' data-fresco-group='web'>
+		  		<img src="images/retina/bobs-red-mill-pie-page.jpg" title="Bob's Red Mill Pie crust Promotion" alt="Bob's Red Mill Pie crust Promotion">
+  			</a><br /><br />
+			<h1>Bob's Red Mill Pie crust Promotion</h1>
+		  	<h3>website design and development</h3>
+		  	<h4>HTML, CSS, PHP</h4>
+  		</div>	
+					 
+		<div id="piece-wrapper">
+  			<a href='images/retina/portland-marathon.jpg' class='fresco' data-fresco-group='web'>
+		  		<img src="images/retina/portland-marathon.jpg" title="Portland Marathon" alt="Portland Marathon">
+  			</a><br /><br />
 			<h1>Portland Marathon Design Composition</h1>
 		  	<h3>website design</h3>
 		  	<h4>Sadly, this design comp. was never used; However, they clearly borrowed from it and did a slight re-design.</h4>
   		</div>
-  	</div>
-
-  	<div class="col span_1_of_2">
+  		
   		<div id="piece-wrapper">
-			<a href="http://www.newgameplus.co" alt="NewGame+" target="blank"><img src="images/450px_images/new-game-plus.jpg" title="NewGame+" alt="NewGame+"></a><br /><br />
-			<h1>NewGame+</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.newgameplus.co" alt="NewGame+" target="blank">http://www.newgameplus.co</a></h4>
-  		</div>
-  	</div>
-	
-	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-			<a href="http://www.bobsredmill.com/pie" alt="Gluten Free Pie Crust Mix" target="blank"><img src="images/550px_images/pie-crust-mix-site.jpg" title="Gluten Free Pie Crust Mix" alt="Gluten Free Pie Crust Mix"></a><br /><br />
-			<h1>Gluten Free Pie Crust Mix Site</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.bobsredmill.com/pie" alt="Gluten Free Pie Crust Mix" target="blank">http://www.bobsredmill.com/pie</a></h4>
-  		</div>
-	</div>
-  	
-  	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-		  	<a href="http://www.bobsredmill.com/oatmeal/" alt="Oatmeal Site" target="blank"><img src="images/550px_images/oatmeal-site.jpg" title="Oatmeal Site" alt="Oatmeal Site"></a><br /><br />
-			<h1>World's Best Oatmeal Campaign Site</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.bobsredmill.com/oatmeal/" alt="Oatmeal Site" target="blank">http://www.bobsredmill.com/oatmeal/</a></h4>
-  		</div>
-  	</div>
-		  	
-	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-			<a href="http://www.grainsofdiscovery.com" alt="Grains of Discovery" target="blank"><img src="images/550px_images/Grains-of-Discovery.jpg" title="Grains of Discovery" alt="Grains of Discovery"></a><br /><br />
-			<h1>Grains of Discovery</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.grainsofdiscovery.com" alt="NewGame+" target="blank">http://www.grainsofdiscovery.com</a></h4>
-  		</div>
-	</div>
-
-	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-			<a href="http://www.sparforthespurtle.com" alt="Spar for the Spurtle" target="blank"><img src="images/550px_images/Spar-for-the-Spurtle.jpg" title="Spar for the Spurtle" alt="Spar for the Spurtle"></a><br /><br />
-			<h1>Spar for the Spurtle</h1>
-		  	<h3>website design/development</h3>
-		  	<h4><a href="http://www.sparforthespurtle.com" alt="NewGame+" target="blank">http://www.sparforthespurtle.com</a></h4>
-  		</div>
-	</div>
-		  	
-	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">	  	
-			<a href="http://offthewallmedia.com/" target="_blank"><img src="http://www.lukemichaels.com/images/offthewallmedia-preview.jpg" title="Off The Wall Media" alt="Off The Wall Media" border="0" /></a><br /><br />
-		    <h1>Off The Wall Media</h1>
-		  	<a href="http://offthewallmedia.com/" target="_blank">http://offthewallmedia.com/</a><br />
-		    <h4>Created in HTML, CSS, JQuery, Wordpress, Illustrator, and Photoshop</h4>
-  		</div>
+  			<a href='images/retina/simple-sushi.jpg' class='fresco' data-fresco-group='web'>
+		  		<img src="images/retina/simple-sushi.jpg" title="Simple Sushi" alt="Simple Sushi">
+  			</a><br /><br />
+			<h1>Simple Sushi</h1>
+		  	<h3>website design & development</h3>
+		  	<h4>HTML, CSS, JavaScript</h4>
+  		</div>			 
+		 
 	</div>
 	
-	<div class="col span_1_of_2">
-  		<div id="piece-wrapper">
-		    <a href="http://www.lukemichaels.com/SimpleSushi" target="_blank"><img src="http://www.lukemichaels.com/images/550px_images/simpleSushiScreengrab.jpg" title="Simple Sushi Preview" alt="Simple Sushi Preview" border="0" /></a><br /><br />
-		  	<h1>Simple Sushi Website</h1>
-		  	<a href="http://www.lukemichaels.com/SimpleSushi" target="_blank">Http://www.simplesushikc.com/</a><br />
-		    <h4>Created in HTML, CSS, Illustrator, and Photoshop</h4>
-  		</div>
-	</div>
-    
-    <div class="col span_1_of_2">
-  		<div id="piece-wrapper">  
-		    <a href="http://www.fiestakc.com/" target="_blank"><img src="http://www.lukemichaels.com/images/550px_images/fiestaKC.jpg" title="fiestakc.com" alt="fiestakc.com" border="0" /></a><br /><br />
-		    <h1>Fiesta KC</h1>
-		     <a href="http://www.fiestakc.com/" target="_blank">http://www.fiestakc.com/</a><br />
-		    <h3>This was a collaboration, I created it while working at a<br />
-		    ad agency and didn't 100% design this site myself.</h3>
-		    <h4>Created in Flash, Java, HTML, CSS, Illustrator, and Photoshop</h4>
-  		</div>
-    </div>
-</div><!--GalleryMain-->
+	<!-- </div> --><!--section group-->
+</div><!--gallery-->
 
 <!-- :::::::::: Footer :::::::::: -->
 <?php include 'includes/footer.php'; ?>
