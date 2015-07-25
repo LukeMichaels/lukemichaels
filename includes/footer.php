4a70679f8ff959af83ee62d84a4709ad1aae17f7
LukeMichaels <lmichaels@gmail.com>
2015-07-25 02:28:36 -0700

    Massive changes to modernize the site a bit, removed Bootstrap because it isn't needed.

--- includes/footer.php
+++ includes/footer.php
@@ -3,38 +3,32 @@
    
    </div><!--wrapper-->
    
-   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
     <script src="js/modernizr-2.5.3.min.js"></script>
     <script src="js/jquery.rwdImageMaps.min.js"></script>
     <script src="js/jquery.imagesloaded.js"></script>
-     <script src="js/jquery.colorbox-min.js"></script>
-     <script src="js/fresco.js"></script>
-    
-    <!-- :::::::::: Masonry :::::::::: -->
-    <!-- <script src="js/masonry.js"></script> -->
+    <script src="js/jquery.colorbox-min.js"></script>
+    <script src="js/fresco.js"></script>
     
     <script type="text/javascript">
-		$(document).ready(function(e) {
+		  $(function() {
 				    
 		    /* :::::::::: dynamic image maps :::::::::: */
 		    $('img[usemap]').rwdImageMaps();
 		    $('area').on('click', function() {
-				/* alert($(this).attr('alt') + ' clicked'); */
-			});
-			
-			/* :::::::::: Sticky Nav :::::::::: */
-			$("#stickyheader").hide()
-			$(window).scroll(function(){
-			      if($(window).scrollTop()>100){
-			         $("#stickyheader").fadeIn();
-			      }else{
-			         $("#stickyheader").fadeOut();
-			      }
-			
-			});
+				  /* alert($(this).attr('alt') + ' clicked'); */
+			  });
+
+        /* :::::::::: Show/Hide Top Nav on Scroll :::::::::: */
+        $(window).scroll(function () {
+        if ( $(this).scrollTop() > 200 && !$('#stickyheader').hasClass('open') ) {
+          $('#stickyheader').addClass('open');
+          $('#stickyheader').slideDown();
+         } else if ( $(this).scrollTop() <= 200 ) {
+          $('#stickyheader').removeClass('open');
+          $('#stickyheader').slideUp();
+        }
+      });
 			
 	    })(jQuery);
     </script>
